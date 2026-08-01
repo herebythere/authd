9cd340823fe77264f3c98b78a4b43221d940ee0a
Taylor Vann <brian.t.vann@gmail.com>
2026-07-31 17:55:27 -0700

    start api keys

--- authd-rs/type_flyweight/src/api_keys.rs
+++ authd-rs/type_flyweight/src/api_keys.rs
@@ -5,17 +5,6 @@ pub struct ApiKey {
     pub id: u64,
     pub organization_id: u64,
     pub people_id: u64,
-    pub key: String,
-    pub title: String,
-    pub lifetime: u64,
-    pub deleted_at: Option<u64>,
-}
-
-#[derive(Clone, PartialEq, Serialize, Deserialize, Debug)]
-pub struct ApiKeyDetails {
-    pub id: u64,
-    pub organization_id: u64,
-    pub people_id: u64,
     pub title: String,
     pub lifetime: u64,
     pub deleted_at: Option<u64>,
