ffa434f7c09f92f43c1f8a502f1f18e4d741f14a
Taylor Vann <brian.t.vann@gmail.com>
2026-08-01 15:10:27 -0700

    add a paginated read by organization

--- authd-rs/sqlite_interfaces/src/people.rs
+++ authd-rs/sqlite_interfaces/src/people.rs
@@ -124,6 +124,55 @@ pub fn read_by_id(conn: &mut Connection, id: i64) -> Result<Option<Person>, Sqli
     Ok(None)
 }
 
+pub struct ReadByOrganizationParams {
+    organization_id: i64,
+    offset: i64,
+    limit: i64,
+}
+
+pub fn read_by_organization(
+    conn: &mut Connection,
+    params: &ReadByOrganizationParams,
+) -> Result<Vec<Result<Person, SqliteInterfaceError>>, SqliteInterfaceError> {
+    let mut stmt = match conn.prepare(
+        "
+        SELECT
+            *
+        FROM
+            people
+        WHERE
+			deleted_at IS NULL
+            AND
+            organization_id = ?1
+        LIMIT
+            ?2
+        OFFSET
+            ?3
+        ",
+    ) {
+        Ok(stmt) => stmt,
+        Err(e) => return Err(SqliteInterfaceError::Rusqlite(e)),
+    };
+
+    let entry_iter = match stmt.query_map(
+        (params.organization_id, params.limit, params.offset),
+        get_entry_from_row,
+    ) {
+        Ok(entry_iter) => entry_iter,
+        Err(e) => return Err(SqliteInterfaceError::Rusqlite(e)),
+    };
+
+    let mut entries: Vec<Result<Person, SqliteInterfaceError>> = Vec::new();
+    for entry in entry_iter {
+        match entry {
+            Ok(ntry) => entries.push(Ok(ntry)),
+            Err(e) => entries.push(Err(SqliteInterfaceError::Rusqlite(e))),
+        }
+    }
+
+    Ok(entries)
+}
+
 // explicit patch (no setting null / option) (never set password_hash_results)
 pub struct PatchParams {
     pub id: i64,
